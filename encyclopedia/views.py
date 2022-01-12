94ca5a93ac749863b08207af3ca4214e4d4ae315
matin bahadoran <matinbhdrn77@gmail.com>
2022-01-12 02:55:51 -0800

    adding EntryPageView

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,27 @@
+from typing_extensions import Self
 from django.shortcuts import render
+from django.views.generic.base import TemplateView, View
+from django.views.generic import DetailView
+from markdown_deux import markdown
 
 from . import util
 
 
-def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+class IndexView(TemplateView):
+    template_name = "encyclopedia/index.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["entries"] = util.list_entries()
+        return context
+
+
+class EntryPageViw(View):
+    def get(self, request, title):
+        entry = util.get_entry(title) #str
+        return render(request, "encyclopedia/entry-page.html", {
+            "entry":markdown(entry),
+            "title":title
+        })
+        
+        
