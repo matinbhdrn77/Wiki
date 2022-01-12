0ab4c92991fe6f96b9a1fee8770185b8c070061e
matin bahadoran <matinbhdrn77@gmail.com>
2022-01-12 03:41:48 -0800

    creat search bar

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
-    path("wiki/<str:title>", views.EntryPageViw.as_view(), name="entry-page")
+    path("wiki/<str:title>", views.EntryPageViw.as_view(), name="entry-page"),
+    path("search-result/", views.SearchView.as_view(), name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,8 +1,11 @@
+import re
 from typing_extensions import Self
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic.base import TemplateView, View
 from django.views.generic import DetailView
 from markdown_deux import markdown
+from django.urls import reverse
 
 from . import util
 
@@ -18,10 +21,26 @@ class IndexView(TemplateView):
 
 class EntryPageViw(View):
     def get(self, request, title):
-        entry = util.get_entry(title) #str
+        entry = util.get_entry(title)  # str
+        if entry == None:
+            entry = "# This encyclopedia doesn't exist"
         return render(request, "encyclopedia/entry-page.html", {
-            "entry":markdown(entry),
-            "title":title
+            "entry": markdown(entry),
+            "title": title
         })
+
+
+class SearchView(View):
+    def post(self, request):
+        search_input = request.POST["q"].lower()
         
-        
+        if search_input in util.list_entries():
+            return HttpResponseRedirect(reverse("entry-page", args=[search_input]))
+
+        same_entries = []
+        for entry in util.list_entries():
+            if search_input in entry.lower():
+                same_entries.append(entry)
+        return render(request, "encyclopedia/search.html", {
+            "entries": same_entries,
+        })
