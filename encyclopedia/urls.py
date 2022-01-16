a0d6ee5812919e56d15feb1b171b60935743e882
matin bahadoran <matinbhdrn77@gmail.com>
2022-01-15 22:19:23 -0800

    create edite

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("wiki/<str:title>", views.EntryPageViw.as_view(), name="entry-page"),
     path("search-result/", views.SearchView.as_view(), name="search"),
-    path("create-page/", views.CreateView.as_view(), name="create-page")
+    path("create-page/", views.CreateView.as_view(), name="create-page"),
+    path("edite-page/<str:title>", views.EditePageView.as_view(), name="edite-page"),
+    path("random/", views.RandomPageView.as_view(), name="random-page")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,9 +1,10 @@
 import re
+from turtle import title
 from typing_extensions import Self
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic.base import TemplateView, View
-from django.views.generic import DetailView
+from django.views.generic import DetailView, UpdateView
 from django.views.generic.edit import FormView
 from markdown_deux import markdown
 from django.urls import reverse
@@ -21,21 +22,25 @@ class IndexView(TemplateView):
         return context
 
 
-class EntryPageViw(View):
-    def get(self, request, title):
+class EntryPageViw(TemplateView):
+    template_name = "encyclopedia/entry-page.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        title = self.kwargs["title"]
+        print(title)
         entry = util.get_entry(title)  # str
         if entry == None:
             entry = "# This encyclopedia doesn't exist"
-        return render(request, "encyclopedia/entry-page.html", {
-            "entry": markdown(entry),
-            "title": title
-        })
+        context["entry"] = markdown(entry)
+        context["title"] = title
+        return context
 
 
 class SearchView(View):
     def post(self, request):
         search_input = request.POST["q"].lower()
-        
+
         if search_input in util.list_entries():
             return HttpResponseRedirect(reverse("entry-page", args=[search_input]))
 
@@ -51,11 +56,23 @@ class SearchView(View):
 class CreateView(FormView):
     template_name = 'encyclopedia/create-page.html'
     form_class = CreatePageForm
-    
 
     def form_valid(self, form):
         title = form.cleaned_data["title"]
         content = form.cleaned_data["contentMd"]
         util.save_entry(title, content)
-        return HttpResponseRedirect(reverse("entry-page", args = [title]))
+        return HttpResponseRedirect(reverse("entry-page", args=[title]))
+
+
+class EditePageView(TemplateView):
+    template_name = "encyclopedia/create-page.html"
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        title = self.kwargs["title"]
+        content = util.get_entry(title)
+        form = CreatePageForm(initial={"contentMd":content, "title":title})
+        context["title"] = title
+        context["form"] = form
+        context["edite"] = True
+        return context
